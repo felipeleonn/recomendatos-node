16e022ccefa57a1bafa24a67ce882fd97997c6d1
felipeleonn <felipeleon98@outlook.com>
2024-10-07 13:52:53 -0300

    feat: Config env

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const PORT = process.env.PORT || 3000;
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const SUPABASE_URL = process.env.SUPABASE_URL!;
+export const SUPABASE_KEY = process.env.SUPABASE_KEY!;
+export const FRONTEND_URL = process.env.FRONTEND_URL!;
+export const BACKEND_URL = process.env.BACKEND_URL!;
+export const MERCADOPAGO_ACCESS_TOKEN = process.env.MERCADOPAGO_ACCESS_TOKEN!;
+export const MERCADOPAGO_CLIENT_ID = process.env.MERCADOPAGO_CLIENT_ID!;
+export const MERCADOPAGO_CLIENT_SECRET = process.env.MERCADOPAGO_CLIENT_SECRET!;
+export const MERCADOPAGO_REDIRECT_URI = process.env.MERCADOPAGO_REDIRECT_URI!;
--- src/services/supabaseService.ts
+++ src/services/supabaseService.ts
@@ -1,9 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+import { SUPABASE_URL, SUPABASE_KEY } from '../config/config';
 
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABASE_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
