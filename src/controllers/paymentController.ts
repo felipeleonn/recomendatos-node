ce84fc19d39530844bb380bdeb9cfaa8e7f842c0
Rodri Marchese <rodrimarchese@gmail.com>
2024-10-16 10:36:24 -0300

    testing redirect url after success payment

--- src/controllers/paymentController.ts
+++ src/controllers/paymentController.ts
@@ -3,7 +3,10 @@ import { updateOrderStatus } from '../services/orderService';
 import { logger } from '../utils/logger';
 
 export const handlePaymentResult = async (req: Request, res: Response) => {
-  const { preference_id, status } = req.query;
+  //  TODO: necesitamos el userId del que paga, que mp lo mande con el back URL
+  const { preference_id, status, usuario_id } = req.query;
+
+  logger.info(`Handling payment result: ${usuario_id}`);
 
   if (!preference_id || !status) {
     return res.status(400);
@@ -12,8 +15,14 @@ export const handlePaymentResult = async (req: Request, res: Response) => {
   try {
     await updateOrderStatus(preference_id.toString(), status.toString());
 
+    logger.info(
+      `https://app.recomendatos.com/redirect?paymentResult=${status.toString()}&usuario_id=${usuario_id}`,
+    );
+
     // TODO: pantalla en la app para mostrar el resultado del pago
-    res.redirect(`https://app.recomendatos.com/redirect?paymentResult=${status.toString()}`);
+    res.redirect(
+      `https://app.recomendatos.com/redirect?paymentResult=${status.toString()}&usuario_id=${usuario_id}`,
+    );
   } catch (error) {
     logger.error('Error handling payment:', error);
     res.status(500).json({ error: 'Internal Server Error' });
